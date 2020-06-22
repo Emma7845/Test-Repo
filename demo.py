5d2c08c4aa458931e590bba61b81c108468433d8
zyy <zyy7348@163.com>
2020-06-22 16:19:21 -0400

    Initial commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+
+adj_123 = "stupid"
+Name_1 = "Joseph"
+print("Who is the most {}? {}!".format(adj_123,Name_1))
+
+adj_123 = "stupid"
+Name_2 = "Joseph"
+print("Who is the most {}? {}!".format(adj_123,Name_2))
+
+adj_123 = "stupid"
+Name_3  = "Joseph"
+print("Who is the most {}? {}!".format(adj_123,Name_3))
